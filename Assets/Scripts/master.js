a7aaa6bc0943e743f90485e61bd9e42505998ab7
Christian Abdelmassih <chrabd@kth.se>
2018-01-22 18:40:21 +0100

    bugfixes and interaction additions

--- Assets/Scripts/master.js
+++ Assets/Scripts/master.js
@@ -2,21 +2,63 @@ window.Prism = require('prismjs');
 window.CodeFlask = require('codeflask');
 window.$ = require('jquery');
 
+window.requestInvalid = false;
+window.editorCodeObject = {};
+
 window.prepareEditor = function() {
   var editor = document.getElementById('editor');
+  var codeInvalidEl = document.getElementById('code-invalid');
+  var sendButtonEl = document.getElementById('send-button');
 
-  var editorCodeObject = {Title: "Ulysses", Body: "Yet all experience is an arch wherethro'; Gleams that untravell'd world, whose margin fades For ever and for ever when I move"};
-  editor.innerHTML = JSON.stringify(editorCodeObject, undefined, 2);
+  window.editorCodeObject.title = "Ulysses";
+  window.editorCodeObject.body = "Yet all experience is an arch wherethro'; Gleams that untravell'd world, whose margin fades For ever and for ever when I move";
 
-  var flask = new CodeFlask;
-  flask.run('#editor', { language: 'javascript' });
+  var editorString = JSON.stringify(window.editorCodeObject, undefined, 2);
+
+  var flask = new window.CodeFlask;
+  flask.run('#editor', { language: 'json' });
+  flask.update(editorString);
 
   flask.onUpdate(function(code) {
-    currentCode = code;
-    console.log(currentCode);
+    try {
+      JSON.parse(code, undefined, 2)
+      window.editorCodeObject = JSON.parse(code, undefined, 2);
+      editorString = code;
+      codeInvalidEl.classList.remove("show");
+      sendButtonEl.classList.remove("inactive");
+      window.requestInvalid = false;
+    } catch (e) {
+      console.log(e);
+      codeInvalidEl.classList.add("show");
+      sendButtonEl.classList.add("inactive");
+      window.requestInvalid = true;
+    }
   });
 };
 
-$(document).ready(function() {
+window.scrollTo = function(el) {
+  $('html, body').animate({
+      scrollTop: $(el).offset().top-150,
+  }, 500);
+};
 
+$(document).ready(function() {
+  var responseContainer = document.getElementById('response-container');
+  var responseText = document.getElementById('response-text');
+
+  $('#send-button').click(function() {
+    var fieldWhitelist = ['title', 'body'], sendObj = {};
+    fieldWhitelist.forEach(function(field) { sendObj[field] = window.editorCodeObject[field]; });
+
+    $.post('api/ipsums',sendObj, function(data, status){
+      if (status === 'success') {
+        responseText.innerHTML = data || `request OK`;
+        responseContainer.classList.remove("hidden");
+        scrollTo('#response-container');
+      } else {
+        responseContainer.classList.add("hidden");
+      }
+    });
+
+  });
 })
